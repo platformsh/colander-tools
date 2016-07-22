8ac9c58e81255fd953510626621e3497e5312550
Damien Tournoud <damien@platform.sh>
2016-07-22 10:28:49 +0200

    serializable: Add documentation

--- colander_tools/serializable.py
+++ colander_tools/serializable.py
@@ -2,6 +2,40 @@
 def serializable(cls):
     """
     Class decorator: bind a colander schema and a python class.
+
+    When used in a class like this::
+
+        @serializable
+        class Country(object):
+            class Schema(MappingSchema):
+                code = SchemaNode(
+                    String(),
+                )
+
+            def __init__(self, code):
+                self.code = code
+
+    It patches ``Country.Schema`` to serialize to and deserialize from instances of ``Country``,
+    and in addition gives you:
+
+    * A ``Country.deserialize(value, bind_kwargs=None)`` class method;
+    * A ``country.serialize(bind_kwargs=None)`` instance method.
+
+    You can reuse ``Country.Schema`` in other schemas, like this::
+
+        @serializable
+        class City(object):
+            class Schema(MappingSchema):
+                name = SchemaNode(
+                    String(),
+                )
+
+                country = Country.Schema()
+
+            def __init__(self, name, country):
+                self.name = name
+                self.country = country
+
     """
     _patch_serializable_class_schema(cls.Schema, cls)
     _patch_serializable_class(cls)
