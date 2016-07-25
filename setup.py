fbe39464fcb7e9c08e0d0278158e0d83184787ff
Damien Tournoud <damien@platform.sh>
2016-07-25 17:04:01 +0200

    Release 0.7.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here, "README.rst")) as f:
 
 setup(
     name="colander_tools",
-    version="0.6.1",
+    version="0.7.0",
     description="Extensions to `colander`, particularly useful as part of REST API validation.",
     long_description=README,
     license="BSD",
